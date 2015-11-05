89d606f6d04d2a0937b50c820d9da6daf49ffa10
Giancarlo Fusiello <Carlo@Zokni00011010.home>
2015-11-05 21:35:16 +0000

    Added images model and file upload. Added media root and static files root.

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -106,3 +106,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
